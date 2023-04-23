2774ad6c5e7f1aed9a07149d23c183151158cc46
Vishnu <vishnukun954AV@protonmail.com>
2023-04-23 10:59:30 +0530

    Removed border bottom from box plus declared function createGrid.

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+//split calculator box into grids of columns and rows
+function createGrid(18) {
+    //access the box 
+    let box = document.querySelector("#box");
+    box.style.display = "grid";
+    //split it into columns
+    box.style.
+}
