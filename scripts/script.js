5bb8218d8e1534978854fe92a8a0770aa76b1cf3
Vishnu <vishnukun954AV@protonmail.com>
2023-04-23 18:46:31 +0530

    Removed excess css elements added styling to calculator container plus buttons established grid removed function.

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-//split calculator box into grids of columns and rows
-function createGrid(18) {
-    //access the box 
-    let box = document.querySelector("#box");
-    box.style.display = "grid";
-    //split it into columns
-    box.style.
-}
