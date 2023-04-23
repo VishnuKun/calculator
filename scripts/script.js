e1dc0919de21369ce45f38e036053a98356cf096
Vishnu <vishnukun954AV@protonmail.com>
2023-04-23 21:39:33 +0530

    Created add function.

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+//basic arithematic operation functions
+
+//addition
+function add(...args) {
+    let sum = 0;
+    for(let i = 0; i < args.length; i++) {
+        let item = args[i];
+        sum += item;
+    };
+    return sum;
+};
+//subtract
+//multiply
+//divide
