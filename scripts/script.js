5ab491e2da7add0dce62518687e266b67ca732d2
Vishnu <vishnukun954AV@protonmail.com>
2023-04-24 09:10:39 +0530

    Declared new functions for more operations.

--- scripts/script.js
+++ scripts/script.js
@@ -10,16 +10,36 @@ function add(num1, num2) {
 //subtract
 function subtract(num1, num2) {
     return num1 - num2;
-}
+};
 //multiply
 function multiply(num1, num2) {
     return num1 * num2;
-}
+};
 //divide
 function divide(num1, num2) {
     return num1 / num2;
-}
+};
 //takes operator plus two numbers
 function operate(operator, num1, num2) {
     //perform arithmetic operation as per operator
-}
+};
+
+//some more functions 
+
+//clear everything from the display and reset values 
+function allClear() {
+};
+//backspace clear at one element at one time
+function backspace() {
+};
+//adds decimal
+function addDecimal() {
+};
+
+//display values plus solution at the display
+function display() {
+};
+
+//return the solution
+function equals() {
+};
