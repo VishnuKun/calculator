5394beaf8ee2231eca1da1f3a45c4f2b79358c15
Vishnu <vishnukun954AV@protonmail.com>
2023-05-02 21:28:35 +0530

    Changed border color of the calculator, plus background, header, footer and font family plus size of the header.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 //variables for storing operator plus numbers
 let operator = "";
-let num1 = 0;
-let num2 = 0;
-//access the display element
+let num1 = "";
+let num2 = "";
+//access the display elements
 let displayBox = document.querySelector(".display");
 //storing everything that is currently on the display
 let displayValue = "";
@@ -14,33 +14,22 @@ let decimalFlag = true;     //for adding decimal
 let operatorFlag = true;    //for operators
 let numFlag = true;         //for numbers
 
-
-//calling functions after dom content is loaded only
-window.addEventListener("DOMContentLoaded", (e) => {
-    let btn = document.addEventListener("click" , (e) => {
-        //get the id of the event target
-        //display stuff
-        //clear one element at a time when backspace is clicked 
-        //clear everything on clicking AC
-        //give the result plus stop further input adding to the display unless AC is clicked 
-    });
-});
-
 //addition
 function add(num1, num2) {
-    return num1 + num2;
+    return Math.round((num1 + num2)*100)/100;
 };
 //subtract
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Math.round((num1 - num2)*100)/100;;
 };
 //multiply
 function multiply(num1, num2) {
-    return num1 * num2;  
+    return Math.round((num1 * num2)*100)/100;;  
 };
 //divide
 function divide(num1, num2) {
-    return num1 / num2;
+    if (num2 === 0) return "user error - replace user"
+    else return Math.round((num1 / num2)*100)/100;;
 };
 //takes operator plus two numbers
 function operate(operator, num1, num2) {
@@ -48,12 +37,11 @@ function operate(operator, num1, num2) {
     //when operator is "+"
     if (operator === '+') return add(num1,num2);
     //when operator is "-"
-    else if (operator === '-') return subtract(num1,num2);
+    if (operator === '-') return subtract(num1,num2);
     //when operator is "x"
-    else if (operator === 'x') return multiply(num1, num2);
+    if (operator === '×') return multiply(num1, num2);
     //when operator is "/"
-    else if (operator === "/") return divide(num1, num2);
-    else return null;
+    if (operator === "÷") return divide(num1, num2);
 };
 
 //some more functions 
@@ -100,53 +88,105 @@ function backSpace() {
 //adds decimal
 function addDecimal() {
     //check if the flag is true or not
-    if (decimalFlag === true && numFlag === true) {
-        //add decimal to the box on click
-        displayValue = ".";
-        displayBox.append(displayValue);
-        //reset displayValue
-        displayValue = "";
-        //change flags
-        decimalFlag = false;
-        numFlag = false;
+    if (decimalFlag === true) {
+        //check if display contains an operator
+        if (!Number(displayBox.textContent)) {
+            //clear operator from the display
+            displayBox.innerHTML = "";
+            //add decimal to the box on click
+            displayBox.innerHTML = ".";
+            //change flags
+            decimalFlag = false;
+            numFlag = true;
+            operatorFlag = true
+            
+        }
+        //check if display contains a number or empty space
+        else {
+            //add decimal to the box on click
+            displayValue = ".";
+            displayBox.append(displayValue);
+            //reset displayValue
+            displayValue = "";
+            //change flags
+            decimalFlag = false;
+            numFlag = true;
+            operatorFlag = true
+        }
     };
 };
 
 //populate the display when any button is clicked
 function display(item) {
-    //check if numflag is true or not
-    if (numFlag === true) {
-        //when a number is clicked
-        if (typeof item === "number") {
-            //append the clicked item to the displayValue
-            displayValue += item;
-            //show displayValue on the display
-            displayBox.append(displayValue);
-            //change flags 
-            operatorFlag = true;
-            decimalFlag = true;
-            equalFlag = true;
+    //when number is clicked
+    if (typeof item === "number") {
+        //check if flag is true
+        if (numFlag === true) {
+            //if number length >= 10 continue
+            if (displayBox.textContent.length <= 10) {
+                //when display box contains "" or number or decimal
+                if (displayBox.textContent.includes(".") || Number.isInteger(Number(displayBox.textContent)) || displayBox.textContent === "") {
+                    //append item to the display
+                    displayValue += item;
+                    displayBox.append(displayValue);
+                    //reset var
+                    displayValue = ""
+                }
+                //when display contains operator
+                else {
+                    //clear displaybox
+                    displayBox.textContent = "";
+                    //append item to the display
+                    displayValue += item;
+                    displayBox.append(displayValue);
+                }
+
+            }
         };
-    };
+    }
     //when an arithmetic operator is clicked
     if (typeof item === "string") {
         //check operator if true or not
         if (operatorFlag === true) {
-            //save number to num1
-            num1 = parseFloat(displayBox.innerHTML);
-            console.log(num1);
-            //save operator
-            operator = item;
-            //append item to the displayValue
-            displayValue += item;
-            //show displayValue to on the displayBox
-            displayBox.innerHTML = displayValue;
-            //change flags 
-            operatorFlag = false;
-            decimalFlag = true;
-            equalFlag = true;
-            //reset displaybox
-            displayValue = "";
+            //if num1 != 0 && operator != null
+            if (num1!=="" && operator!=="") {
+                //set current screen number to num2
+                num2 = parseFloat(displayBox.textContent);
+                //operate
+                let solution = operate(operator,num1,num2);
+                //display solution on clicking operator
+                displayBox.innerHTML = solution;
+                //reset num variables
+                num1 = solution;
+                num2 = "";
+                operator = "";
+                solution = "";
+                //change flags
+                operatorFlag = true;
+                numFlag = true;
+                decimalFlag = true;
+                equalFlag = true;
+            }
+            //when num1 == null && operator also null
+            else {
+                //save number to num1
+                num1 = parseFloat(displayBox.textContent);
+                //save operator
+                operator = item;
+                //append item to the displayValue
+                displayValue = item;
+                //show displayValue to on the displayBox
+                displayBox.innerHTML = item;
+                //check if num1 and operator is null or not
+                
+                //change flags 
+                operatorFlag = true;
+                decimalFlag = true;
+                equalFlag = true;
+                //reset displaybox
+                displayValue = "";
+                displayBox.append(displayValue);
+            }
         };
     };
     //reset displayValue
@@ -157,24 +197,23 @@ function display(item) {
 function equals() {
     //check if equal flag is true or not
     if (equalFlag === true) {
+        //get num2 from display
+        num2 = parseFloat(displayBox.innerHTML);
         //clear everything from the display
         displayBox.innerHTML = "";
         //perform operation
-        solution = operate(operator, num1, num2);
+        let solution = operate(operator, num1, num2);
         //add the value to displayvalue then to box
-        displayValue += solution;
-        displayBox.append(displayValue);
+        displayBox.textContent = solution;
         //change flags
-        decimalFlag = false;
-        operatorFlag = false;
+        decimalFlag = true;
+        operatorFlag = true;
         equalFlag = false;
-        numFlag = false;
+        numFlag = true;
+        operator = true;
         //reset variables
         displayValue = "";
-        num1 = 0;
-        num2 = 0;
-        operator = true;
+        num1 = "";
+        num2 = "";
     };
 };
-
-//testing
